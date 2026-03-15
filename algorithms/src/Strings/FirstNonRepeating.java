d8ec845de604f9c5fdb1314203c064392d85ea51
OlaAdams <adamsonoladipupo@gmail.com>
2026-03-15 05:20:34 +0100

    new update

---
+++ algorithms/src/Strings/FirstNonRepeating.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class FirstNonRepeating {
+    public static String firstNonReapeating(String word){
+        for (int check = 0; check < word.length(); check++){
+            
+        }
+    }
+}
