58d42097fb66a594e32329c5c0bc480e4af0ebd1
OlaAdams <adamsonoladipupo@gmail.com>
2026-03-16 07:49:30 +0100

    new commit

--- algorithms/src/Strings/AsciiToInteger.java
+++ algorithms/src/Strings/AsciiToInteger.java
@@ -2,6 +2,18 @@ package Strings;
 
 public class AsciiToInteger {
     public static int aito(String input){
+
         return 0;
+    }
+    private static String removeWhiteSpaces(String userInput){
+        String newString = "";
+        for (int remove = 0; remove < userInput.length(); remove++){
+            if (userInput.charAt(remove) != ' '){
+                newString += userInput.charAt(remove);
+            }
+        }return  newString;
+    }
+    public static void main(String[] args){
+        System.out.println(removeWhiteSpaces("  let Getme "));
     }
 }
