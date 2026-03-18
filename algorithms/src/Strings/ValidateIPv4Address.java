4855c879300f2221626c92ae1fb5b497c20e8c77
OlaAdams <adamsonoladipupo>
2026-03-18 06:48:16 +0100

    new commit

---
+++ algorithms/src/Strings/ValidateIPv4Address.java
@@ -0,0 +1,31 @@
+package Strings;
+
+public class ValidateIPv4Address {
+    public static boolean validate(String IPv4Address){
+
+        return false;
+    }
+    public static boolean checkDot(String IPv4Address){
+        int dotCount = 0;
+        for (int check = 0; check < IPv4Address.length(); check ++){
+            if (IPv4Address.charAt(check) == '.') dotCount++;
+        }
+        return dotCount >= 3;
+    }
+    private static boolean checkNumbers(String IPAddress){
+        int[] arrayBoolean = new int[4];
+        String number = "";
+        for (int check = 0; check < IPAddress.length(); check++){
+
+            if (IPAddress.charAt(check) != '.'){
+                number += IPAddress.charAt(check);
+            }
+            int tempVar = Integer.parseInt(number);
+            number = "";
+            System.out.println(tempVar);
+        } return false;
+    }
+    public static void main(String[] args){
+        System.out.println(checkNumbers("128."));
+    }
+}
