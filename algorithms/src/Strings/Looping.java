996e844b14a1f5270947fca05d9edca4b880a015
OlaAdams <adamsonoladipupo@gmail.com>
2026-03-20 05:35:15 +0100

    new commit

---
+++ algorithms/src/Strings/Looping.java
@@ -0,0 +1,7 @@
+package Strings;
+
+public class Looping {
+    public static boolean loop(String input){
+        return false;
+    }
+}
---
+++ algorithms/test/Strings/LoopingTest.java
@@ -0,0 +1,8 @@
+package Strings;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoopingTest {
+    
+
+}
