47009ea94e849937b4ef68b9042f16072528e697
OlaAdams <adamsonoladipupo@gmail.com>
2026-03-20 05:53:40 +0100

    new update

--- algorithms/src/Strings/Pangram.java
+++ algorithms/src/Strings/Pangram.java
@@ -2,10 +2,20 @@ package Strings;
 
 public class Pangram {
     public static boolean isPangram(String input){
+        boolean finalResult = false;
         for (char characters = 'a'; characters <= 'z'; characters++){
-            System.out.println(characters);
+            for (int loop = 0; loop < input.length(); loop++){
+                if (Character.toLowerCase(input.charAt(loop)) == characters){
+                    finalResult = true;
+                    System.out.print("Them be thesame");
+                    return finalResult;
+                }
+                else {
+                    return finalResult;
+                }
+            }
         }
-        return false;
+        return true;
     }
     public static void main(String [] args){
         System.out.println(isPangram("lets get"));
