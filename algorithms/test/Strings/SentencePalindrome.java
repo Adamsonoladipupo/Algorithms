7d3139a45b5789d635352032991996cabed423bb
OlaAdams <adamsonoladipupo>
2026-03-24 06:47:42 +0100

    new commit

--- algorithms/test/Strings/SentencePalindrome.java
+++ algorithms/test/Strings/SentencePalindrome.java
@@ -4,4 +4,18 @@ public class SentencePalindrome {
     public static boolean isPalindrome(String sentence){
         return false;
     }
+    public static String removeNonAlphanumericCharacters(String sentence){
+        String newSentence = "";
+        for (char alphabet = 'a'; alphabet <= 'z'; alphabet++){
+            for (int number = 0; number <= 9; number++){
+                if(sentence.charAt(alphabet) == alphabet ){
+                    newSentence += alphabet;
+                }
+                if(sentence.charAt(alphabet) == number){
+                    newSentence += number;
+                }
+            }
+        }
+        return newSentence;
+    }
 }
---
+++ algorithms/test/Strings/SentencePalindromeTest.java
@@ -0,0 +1,7 @@
+package Strings;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SentencePalindromeTest {
+
+}
