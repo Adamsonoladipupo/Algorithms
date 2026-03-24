3859e46bb95382f45fc8e6ba1e6cdedae4c0f95b
OlaAdams <adamsonoladipupo>
2026-03-24 07:05:51 +0100

    new commit

--- algorithms/test/Strings/SentencePalindrome.java
+++ algorithms/test/Strings/SentencePalindrome.java
@@ -6,16 +6,22 @@ public class SentencePalindrome {
     }
     public static String removeNonAlphanumericCharacters(String sentence){
         String newSentence = "";
-        for (char alphabet = 'a'; alphabet <= 'z'; alphabet++){
-            for (int number = 0; number <= 9; number++){
-                if(sentence.charAt(alphabet) == alphabet ){
+        for (int loop = 0; loop < sentence.length(); loop++){
+            for (char alphabet = 'a'; alphabet <= 'z'; alphabet++){
+                if(sentence.charAt(loop) == alphabet ){
                     newSentence += alphabet;
                 }
-                if(sentence.charAt(alphabet) == number){
+            }
+            for (int number = 0; number <= 9; number++){
+                if(sentence.charAt(loop) == number){
                     newSentence += number;
                 }
             }
         }
         return newSentence;
+    }
+    public static void main(String[] args){
+        String n = removeNonAlphanumericCharacters("Too hot to hoot.");
+        System.out.println(n);
     }
 }
--- algorithms/test/Strings/SentencePalindromeTest.java
+++ algorithms/test/Strings/SentencePalindromeTest.java
@@ -1,7 +1,14 @@
 package Strings;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SentencePalindromeTest {
+    @Test
+    void removeAllNonAlphanumericCharcatersTest(){
+        String sentence = "Too hot to hoot.";
+        System.out.println(SentencePalindrome.removeNonAlphanumericCharacters(sentence));
+    }
 
 }
