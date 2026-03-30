5037dd11fc3a5575f55b8dca0baba17a2f845a99
OlaAdams <adamsonoladipupo>
2026-03-30 08:28:50 +0100

    new commit

--- algorithms/src/Strings/RemovingDuplicate.java
+++ algorithms/src/Strings/RemovingDuplicate.java
@@ -5,27 +5,26 @@ import java.util.Objects;
 
 public class RemovingDuplicate {
 //    Reduce the string by removing K consecutive identical characters
-    public static String removeDuplicate(String word, int numberOfConsecutiveDuplicate){
-        
-        return "";
-    }
-    public static String getDuplicate(String word, int consecutive){
-        int countDuplicate = 0; String duplicate = "";
+    public static String removeDuplicate(String word, int consecutive){
+        int countDuplicate = 0; String duplicate = ""; String newString = "";
         String[] letters = word.split("");
-        System.out.println(Arrays.toString(letters));
         for (int loop = 0; loop < letters.length-1; loop++){
             if (Objects.equals(letters[loop], letters[loop + 1])){
-                duplicate +=letters[loop] + letters[loop+1];
-                countDuplicate = 2;
-//                if(countDuplicate == consecutive){
-//                }
+                for (int innerLoop = loop; innerLoop < letters.length-1; innerLoop++){
+                    if(letters[innerLoop] == letters[loop]){
+                        duplicate += letters[innerLoop];
+                        countDuplicate ++;
+                    }
+                }
+                if (countDuplicate == consecutive) System.out.println("Consecutive");
             }
+            else newString += letters[loop];
         }
-        return duplicate;
+        return newString;
     }
     public static void main(String[] args){
-        String word = "goose";
-        System.out.println(getDuplicate(word, 2));
+        String word = "gooose";
+        System.out.println(removeDuplicate(word, 3));
     }
 
 }
