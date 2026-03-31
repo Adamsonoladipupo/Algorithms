7633a7edf8a0571f39376288dc92d2a7954515e6
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-03-31 06:59:25 +0100

    testing

--- algorithms/src/Summing.java
+++ algorithms/src/Summing.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class Summing {
     public static int singleDigitSum(int firstNumber, int secondNumber){
         return firstNumber + secondNumber;
@@ -6,25 +9,33 @@ public class Summing {
         return number%10;
     }
     public static int sum(int firstNumber, int secondNumber){
-        int fN = getLastNumber(firstNumber);
-        int sN = getLastNumber(secondNumber);
-
-        int result =singleDigitSum(fN, sN);
-
-        return result;
+        return 0;
     }
     public static int getSmallest(int firstNumber, int secondNumber){
         if(firstNumber > secondNumber){
             return secondNumber;
         } else return firstNumber;
     }
-    public static void main(String[] args){
-//        System.out.println(sum(25, 11));
-//        System.out.println(getSmallest(11, 25));
-        int number = 200; int number2 = 100;
-        while(number > 0){
-            System.out.println(number);
-            number = number/10;
+    public static int reverseString(String input){
+        String finalResult = "";
+        for (int reverse = input.length()-1; reverse >= 0; reverse--){
+            finalResult += input.charAt(reverse);
         }
+        return Integer.parseInt(finalResult);
+    }
+    public static void main(String[] args){
+        int number = 10000000; int number2 = 89990000;
+        String result = "";
+        while(number > 0){
+            result += (
+                    singleDigitSum(
+                            getLastNumber(number), getLastNumber(number2)
+                    )
+            );
+            number = number/10;
+            number2 = number2/10;
+        }
+        System.out.println(reverseString(result));
+
     }
 }
