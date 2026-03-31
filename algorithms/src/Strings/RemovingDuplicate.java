1f595ef29daca8e3c04a98c348df98fba0578622
OlaAdams <adamsonoladipupo>
2026-03-31 04:54:17 +0100

    new updare

--- algorithms/src/Strings/RemovingDuplicate.java
+++ algorithms/src/Strings/RemovingDuplicate.java
@@ -6,25 +6,15 @@ import java.util.Objects;
 public class RemovingDuplicate {
 //    Reduce the string by removing K consecutive identical characters
     public static String removeDuplicate(String word, int consecutive){
-        int countDuplicate = 0; String duplicate = ""; String newString = "";
-        String[] letters = word.split("");
-        for (int loop = 0; loop < letters.length-1; loop++){
-            if (Objects.equals(letters[loop], letters[loop + 1])){
-                for (int innerLoop = loop; innerLoop < letters.length-1; innerLoop++){
-                    if(letters[innerLoop] == letters[loop]){
-                        duplicate += letters[innerLoop];
-                        countDuplicate ++;
-                    }
-                }
-                if (countDuplicate == consecutive) System.out.println("Consecutive");
+        String newString = ""; int count = 0;
+        for (int loop =0; loop < word.length()-1; loop++){
+            newString += word.charAt(loop);
+            System.out.println(word.charAt(loop));
+            if(word.charAt(loop) == word.charAt(loop+1)){
+                count++;
             }
-            else newString += letters[loop];
         }
         return newString;
-    }
-    public static void main(String[] args){
-        String word = "gooose";
-        System.out.println(removeDuplicate(word, 3));
     }
 
 }
