9485810d9a0b15dc75e22d3d0591f6e29412e22e
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-04 04:22:56 +0100

    arrays: Longest common prefix

--- algorithms/src/arrays/LongestCommonPrefix.java
+++ algorithms/src/arrays/LongestCommonPrefix.java
@@ -1,5 +1,13 @@
 package arrays;
 
 public class LongestCommonPrefix {
-    
+    public static String longestCommonPrefix(String[]words){
+        String commonPrefix = "";
+        for(int loop = 0; loop < words.length; loop++){
+            for(int checkWord = 0;checkWord< words[loop].length(); checkWord++){
+                if
+            }
+        }
+        return commonPrefix;
+    }
 }
