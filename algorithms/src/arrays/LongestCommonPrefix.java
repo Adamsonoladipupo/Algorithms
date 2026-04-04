e618e5107c6443759df515d32cf743d5802e3c3e
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-04 05:36:55 +0100

    arrays: Longest common prefix

--- algorithms/src/arrays/LongestCommonPrefix.java
+++ algorithms/src/arrays/LongestCommonPrefix.java
@@ -1,13 +1,34 @@
 package arrays;
 
+import java.util.Objects;
+
 public class LongestCommonPrefix {
     public static String longestCommonPrefix(String[]words){
-        String commonPrefix = "";
+        String commonPrefix = ""; String prefix = ""; int count = 1;
+        if(words.length == 1) return words[0];
         for(int loop = 0; loop < words.length; loop++){
-            for(int checkWord = 0;checkWord< words[loop].length(); checkWord++){
-                if
+            prefix = getPrefix(getWord(loop, words), count);
+            for (int innerLoop = 0; innerLoop < words.length; innerLoop++){
+                if(Objects.equals(prefix, getPrefix(getWord(innerLoop, words), count))){
+                    if(innerLoop == loop) continue;
+                    commonPrefix = prefix;
+                    count++;
+                } else if (innerLoop != loop){
+                    break;
+                }
             }
         }
         return commonPrefix;
+    }
+    public static String getWord(int arrayIndex, String[]array){
+            return array[arrayIndex];
+    }
+    public static String getPrefix(String word, int prefixCount){
+        StringBuilder prefix = new StringBuilder();
+        if(Objects.equals(word, ""))return "";
+        for (int loop = 0; loop < prefixCount; loop++){
+            prefix.append(word.charAt(loop));
+        }
+        return prefix.toString();
     }
 }
---
+++ algorithms/test/arrays/LongestCommonPrefixTest.java
@@ -0,0 +1,61 @@
+package arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LongestCommonPrefixTest {
+    @Test
+    void LongestCommonPrefixGetWordFromArrayTest(){
+        String[] input = {"flower","flow","flight"};
+        String expected = LongestCommonPrefix.getWord(0,input);
+        assertEquals("flower", expected);
+    }
+
+    @Test
+    void LongestCommonPrefixGetPrefixFromWordTest(){
+        String word = "flower";
+        String prefix = LongestCommonPrefix.getPrefix(word, 2);
+        assertEquals("fl", prefix);
+    }
+
+    @Test
+    void LongestCommonPrefixTest(){
+        String[] input = {"flower","flow","floght, flock"};
+        String expected = "flo";
+        String actual = LongestCommonPrefix.longestCommonPrefix(input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void LongestCommonPrefixTwoTest(){
+        String[] input = {"dog","racecar","car"};
+        String expected = "";
+        String actual = LongestCommonPrefix.longestCommonPrefix(input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void LongestCommonPrefixThreeTest(){
+        String[] input = {""};
+        String expected = "";
+        String actual = LongestCommonPrefix.longestCommonPrefix(input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void LongestCommonPrefixFourTest(){
+        String[] input = {"a"};
+        String expected = "a";
+        String actual = LongestCommonPrefix.longestCommonPrefix(input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void LongestCommonPrefixFiveTest(){
+        String[] input = {"ab", "a"};
+        String expected = "a";
+        String actual = LongestCommonPrefix.longestCommonPrefix(input);
+        assertEquals(expected, actual);
+    }
+}
